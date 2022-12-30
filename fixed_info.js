5e355916e96eac29e55e49f06246666852179f21
Quni <71239646+Kuni-Yoshi@users.noreply.github.com>
2022-12-30 16:42:39 +0800

    type_and_serial Initial
    初期値

--- fixed_info.js
+++ fixed_info.js
@@ -1,3 +1,3 @@
-var type_and_serial = "No serial number";
+var type_and_serial = "Please connect to MQTT broker and get serial number";
 var dne_version = "Not included in sample files.";
 var dne_option = "Not included in sample files.";
